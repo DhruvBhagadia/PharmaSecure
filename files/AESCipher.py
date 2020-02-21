28b297363ac6f9ff2690fd830da07f00f9f1e4e5
DhruvBhagadia <dhruvbhagadia@gmail.com>
2020-02-21 11:57:52 +0530

    added methods to encrypt/decrypt a file

--- files/AESCipher.py
+++ files/AESCipher.py
@@ -2,9 +2,6 @@ import os
 from Crypto.Cipher import AES
 from Crypto import Random
 
-
-
-
 def gen_key():
     BS = 16
     key = os.urandom(BS)
@@ -26,5 +23,46 @@ def decrypt(key,ciphertext):
     cipher = AES.new(key, AES.MODE_CBC, iv)
     plaintext = cipher.decrypt(ciphertext[AES.block_size:])
     return plaintext.decode('utf8').strip("0")
+
+def encrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
+    if not out_filename:
+        out_filename = in_filename + '.enc'
+
+    iv = Random.new().read(AES.block_size)
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    filesize = os.path.getsize(in_filename)
+
+    with open(in_filename, 'rb') as infile:
+        with open(out_filename, 'wb') as outfile:
+            outfile.write(struct.pack('<Q', filesize))
+            outfile.write(iv)
+
+            while True:
+                chunkBytes = infile.read(chunksize)
+                chunk = chunkBytes.decode("utf-8")
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += ' ' * (16 - len(chunk) % 16)
+
+                outfile.write(encryptor.encrypt(chunk.encode("utf-8")))
+
+def decrypt_file(key, in_filename, out_filename=None, chunksize=24*1024):
+    if not out_filename:
+        out_filename = 'decrypt.txt'
+
+    with open(in_filename, 'rb') as infile:
+        origsize = struct.unpack('<Q', infile.read(struct.calcsize('Q')))[0]
+        iv = infile.read(16)
+        decryptor = AES.new(key, AES.MODE_CBC, iv)
+
+        with open(out_filename, 'wb') as outfile:
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                outfile.write(decryptor.decrypt(chunk))
+
+            outfile.truncate(origsize)
 
 
